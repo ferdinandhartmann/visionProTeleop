cd0d056b96d8df83394394893c9ad5308505b19c
Hayato-80 <jojohaya.fs.sb421@outlook.jp>
2025-12-09 11:20:18 +0900

    Added a brige but not tested yet

--- ros2_ws/src/teleoperation/setup.py
+++ ros2_ws/src/teleoperation/setup.py
@@ -27,6 +27,7 @@ setup(
             "transform_publisher = teleoperation.transform_publisher:main",
             "mycobot_adapter = teleoperation.mycobot_adapter:main",
             "teleop_control = teleoperation.teleop_control:main",
+            "teleop_coords_bridge = teleoperation.teleop_bridge:main",
         ],
     },
 )
